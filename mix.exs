d9b711b066c20bea2f9970c4a25ea9e3f2dc32be
Adrian Ibañez <hello@adrianibanez.info>
2025-02-21 23:03:29 +0100

    trying to make linux target install pkgs

--- mix.exs
+++ mix.exs
@@ -2,6 +2,9 @@ defmodule RustlerBtleplug.MixProject do
   use Mix.Project
 
   @version "0.0.1-alpha"
+  @source_url "https://github.com/adiibanez/rustler_btleplug"
+  @dev? String.ends_with?(@version, "-dev")
+  @force_build? System.get_env("BTLEPLUG_BUILD") in ["1", "true"]
 
   @nerves_rust_target_triple_mapping %{
     "armv6-nerves-linux-gnueabihf": "arm-unknown-linux-gnueabihf",
@@ -34,10 +37,25 @@ defmodule RustlerBtleplug.MixProject do
 
     [
       app: :rustler_btleplug,
-      version: "0.1.0",
+      name: "Rustler btleplug",
+      description: "Allows ble communication between elixir and rust",
+      version: @version,
       elixir: "~> 1.18",
-      start_permanent: Mix.env() == :prod,
+      # elixirc_paths: elixirc_paths(Mix.env()),
+      package: package(),
       deps: deps(),
+      # docs: docs(),
+      preferred_cli_env: [ci: :test],
+      aliases: [
+        "rust.lint": [
+          "cmd cargo clippy --manifest-path=native/btleplug_client/Cargo.toml -- -Dwarnings"
+        ],
+        "rust.fmt": ["cmd cargo fmt --manifest-path=native/btleplug_client/Cargo.toml --all"],
+        # "localstack.setup": ["cmd ./test/support/setup-localstack.sh"],
+        ci: ["format", "rust.fmt", "rust.lint", "test"],
+        fmt: ["format", "rust.fmt"]
+      ],
+      start_permanent: Mix.env() == :prod,
       dynamic_library_extension: :dylib
     ]
   end
@@ -70,21 +88,6 @@ defmodule RustlerBtleplug.MixProject do
       {:rustler_precompiled, "~> 0.7"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-    ]
-  end
-
-  defp aliases do
-    [
-      "rust.lint": [
-        "cmd cargo clippy --manifest-path=native/btleplug_client/Cargo.toml -- -Dwarnings"
-      ],
-      "rust.fmt": ["cmd cargo fmt --manifest-path=native/btleplug_client/Cargo.toml --all"],
-      # "localstack.setup": ["cmd ./test/support/setup-localstack.sh"],
-      ci: ["format", "rust.fmt", "rust.lint", "test"],
-      fmt: [
-        "format",
-        "rust.fmt"
-      ]
     ]
   end
 end

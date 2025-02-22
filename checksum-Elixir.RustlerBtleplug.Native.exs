6d0caba24d1453f7dcffba25bf1578071065ba3b
Adrian Ibañez <hello@adrianibanez.info>
2025-02-22 05:56:56 +0100

    add checksum

---
+++ checksum-Elixir.RustlerBtleplug.Native.exs
@@ -0,0 +1,9 @@
+%{
+  "btleplug_client-v0.0.1-alpha-nif-2.15-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:3369975ec7d874e7c4caeefaabb4f15e9075c4354bb5d0f423a4b27731e4fb3f",
+  "libbtleplug_client-v0.0.1-alpha-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:e47631b6b0c31ae01a25a78ee4048083dd28020b2768378e3bedbb9bb7701411",
+  "libbtleplug_client-v0.0.1-alpha-nif-2.15-aarch64-unknown-linux-musl.so.tar.gz" => "sha256:9f1f68079321a153abc13bd177a35a75772af8d5ded9aaa007558bb42b970854",
+  "libbtleplug_client-v0.0.1-alpha-nif-2.15-arm-unknown-linux-gnueabihf.so.tar.gz" => "sha256:0e414891337f5d1d2471b3b64281ef5d0dcb315e2c2670c18f21dadebb113af2",
+  "libbtleplug_client-v0.0.1-alpha-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:b42890221f8fbb9ed67d8eb31252f26c128f58805bf1e216aa3088975008e1b0",
+  "libbtleplug_client-v0.0.1-alpha-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:7add818719c6faed1486d4355a5c52a85febc23ef44cae04002055b399d03d0f",
+  "libbtleplug_client-v0.0.1-alpha-nif-2.15-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:28a417c3cae2b78406dda0a6bffca975744260c372a0f005873cadff03a845b4",
+}

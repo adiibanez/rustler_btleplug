882b5c24155ed003d1dbaf0921d81e4362ed1817
Adrian Ibañez <hello@adrianibanez.info>
2025-02-22 23:46:59 +0100

    add precompiled checksums

--- checksum-Elixir.RustlerBtleplug.Native.exs
+++ checksum-Elixir.RustlerBtleplug.Native.exs
@@ -1,10 +1,10 @@
 %{
-  "btleplug_client-v0.0.1-alpha-nif-2.15-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:86583ca924d39cbb5fbac6d0fe55ee5a40f8755f6991be900f853ae6fb6fc3d4",
-  "libbtleplug_client-v0.0.1-alpha-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:5bd52eeb734fcd219d4262946e59c4465356b6f91b28a383db1dd90ea4237f74",
-  "libbtleplug_client-v0.0.1-alpha-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:a7f93760a24cf14bb1cec2c4bf9907e239497c309d26ab61d704dc9d233840d1",
-  "libbtleplug_client-v0.0.1-alpha-nif-2.15-aarch64-unknown-linux-musl.so.tar.gz" => "sha256:42ae031bad66d258e593c1669dfe45c692944de9160e3aec855cc1365bfab593",
-  "libbtleplug_client-v0.0.1-alpha-nif-2.15-arm-unknown-linux-gnueabihf.so.tar.gz" => "sha256:252e29cac356d77bdf22c49ca701d67b4927eab7b5a080d86efda8b056ce4f5a",
-  "libbtleplug_client-v0.0.1-alpha-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:3a4141e1dfc208bf9d91221c5a76bd34eae270790e9268e0a1ca6d94908c2f97",
-  "libbtleplug_client-v0.0.1-alpha-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:f0ce052b2529a01d484b4f31bae140c5064dc1627e5949994f54e42fa22b5017",
-  "libbtleplug_client-v0.0.1-alpha-nif-2.15-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:19a4307e585b9a0724f7a0fd81d0bcc170b951c1977c91082bf57a1c39b1de98",
+  "btleplug_client-v0.0.1-alpha-nif-2.15-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:69992d15e0cf8f00bdf17d8804c05c359e7a47554d1c4d01a4967c40b8a7fe5c",
+  "libbtleplug_client-v0.0.1-alpha-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:5f76f4c2df8687633eb6c63c35a6e453140bcf61fb4611457f8e1f4f34712e57",
+  "libbtleplug_client-v0.0.1-alpha-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:30ddb939fa52ee257ec4f515147e1dc297424d5e4c05185d42a7e46993c3f4bc",
+  "libbtleplug_client-v0.0.1-alpha-nif-2.15-aarch64-unknown-linux-musl.so.tar.gz" => "sha256:d23616d6e08991e27202b61ebf9a09ee03bdda585d4c63c6f6d1a002ba8b5966",
+  "libbtleplug_client-v0.0.1-alpha-nif-2.15-arm-unknown-linux-gnueabihf.so.tar.gz" => "sha256:097ddde67e5d315049f0bdc48735b19873b3c149b1192ac150dd7b8d42cf6b8b",
+  "libbtleplug_client-v0.0.1-alpha-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:9c432abb9ca67d0cf95f5573408cc1aa60c999928f1b3aeca26476d55cebae2a",
+  "libbtleplug_client-v0.0.1-alpha-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:c4bcabb61a25cf1c1b2f2dfda7cc6c27a855962d5d3a1ca4f4fee64161df7980",
+  "libbtleplug_client-v0.0.1-alpha-nif-2.15-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:7c87ec787702278e28165cfd1525c339df8b47bb66341b325c4a5601196489c6",
 }

c25fc4a82bebf8c64ed1a4d4b9a6ef26f9d12522
Adrian Ibañez <hello@adrianibanez.info>
2025-02-24 01:47:53 +0100

    cleanup

--- docs/git-roundtrip.sh
+++
@@ -1,8 +0,0 @@
-#git push --delete origin v0.0.1-alpha
-#git tag -d v0.0.1-alpha
-git add mix.*
-git add .github/
-git add native/
-git commit -m 'hex roundtrip'
-git tag v0.0.7-alpha
-git push --tags
